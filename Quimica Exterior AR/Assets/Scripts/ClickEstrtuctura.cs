efd71baf6df054f6f4e8319f2a2f54e973af65b7
javierSpector <56440542+javierSpector@users.noreply.github.com>
2019-11-05 15:50:18 -0300

    varios

---
+++ Quimica Exterior AR/Assets/Scripts/ClickEstrtuctura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickEstrtuctura : MonoBehaviour
+{
+
+   public GameObject mostrar;
+
+   private void Start()
+   {
+      mostrar.SetActive(false);
+   }
+
+   void OnMouseDown()
+   {
+      mostrar.SetActive(true);
+   }
+}
