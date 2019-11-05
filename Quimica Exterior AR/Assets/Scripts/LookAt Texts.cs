5716c38f221840dfd74a2dbe77070b6101546ecb
javierSpector <56440542+javierSpector@users.noreply.github.com>
2019-11-05 16:10:52 -0300

    script

---
+++ Quimica Exterior AR/Assets/Scripts/LookAt Texts.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTexts : MonoBehaviour
+{
+    public GameObject target;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(target.transform.position.x,transform.position.y, target.transform.position.z);
+        
+        transform.LookAt(targetPosition);
+    }
+}
