c23bc99247baf28c607dedd57ad62f88df829a06
javierSpector <56440542+javierSpector@users.noreply.github.com>
2019-11-21 11:08:49 -0300

    Scripts

---
+++ Quimica Exterior AR/Assets/clickRV.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickRV : MonoBehaviour
+{
+    public GameObject mostrar;
+    private bool status;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        status = false;
+        mostrar.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        status = true;
+        mostrar.SetActive(true);
+    }
+}
