88dee34d0098a77989720c0abdeca8068b17c08e
javierSpector <56440542+javierSpector@users.noreply.github.com>
2019-11-21 10:01:14 -0300

    Animation Script

---
+++ Quimica Exterior AR/Assets/tansitionAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tansitionAnimation : MonoBehaviour
+{
+    Animator anim;
+    public int counter;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            counter++;
+            
+            anim.SetInteger("animationTransition", counter);
+        }
+        
+        
+    }
+}
