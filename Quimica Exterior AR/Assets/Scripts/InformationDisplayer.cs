433a8badad5981502139e34f6009e24f9de6a626
Franco Palumbo <franco.palumbo@ing.austral.edu.ar>
2019-12-20 15:05:09 -0300

    Add swipable photo galery to plaza de transferencia

---
+++ Quimica Exterior AR/Assets/Scripts/InformationDisplayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+
+public class InformationDisplayer : MonoBehaviour
+{
+    public GameObject canvas;
+    public Button closeImage;
+    private Swipe swipe;
+    
+    private bool status;
+    private void Start()
+    {
+        status = false;
+        canvas.SetActive(status);
+
+        closeImage.onClick.AddListener(closeCanvas);
+        swipe = FindObjectOfType<Swipe>();
+    }
+
+    void OnMouseDown()
+    {
+        if (status) return;
+        status = true;
+        swipe.gameObject.SetActive(!status);
+        canvas.SetActive(status);
+        
+    }
+
+    private void closeCanvas() {
+        status = false;
+        canvas.SetActive(false);
+        swipe.gameObject.SetActive(!status);
+    }
+}
