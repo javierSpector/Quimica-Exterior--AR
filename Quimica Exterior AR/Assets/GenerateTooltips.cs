1f1286c9bc254de96b5f381bf8a8cb01955fadcb
Franco Palumbo <palumbofranco9a@gmail.com>
2019-12-22 16:25:44 -0300

    Add tooltips

---
+++ Quimica Exterior AR/Assets/GenerateTooltips.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class GenerateTooltips : MonoBehaviour
+{
+
+    [FormerlySerializedAs("startPos")] public Transform startPlane;
+
+    private LineRenderer _lineRenderer;
+
+    private float width = 0.25f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.startWidth = _lineRenderer.endWidth = width;
+        _lineRenderer.material = startPlane.gameObject.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
