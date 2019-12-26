2ee65dffe6bfec21922c733e6296bda1241623f7
Franco Palumbo <franco.palumbo@ing.austral.edu.ar>
2019-12-26 13:58:36 -0300

    Add Hospital building, change photos and video

---
+++ Quimica Exterior AR/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface CanvasManager {
+
+    void ShowOptions();
+    void ShowInformation();
+    void ShowPhotoGalery();
+    void ShowVideo();
+    void Close();
+
+
+}
